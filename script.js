3b830eda0b913796f3299bb847e940c1045849e9
santhosh <86908472+sandymangit0000@users.noreply.github.com>
2024-11-03 01:38:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+
+
 window.addEventListener("scroll", function() {
     const header = document.querySelector("header");
-    if (window.scrollY > this.window.innerHeight) { // Adjust scroll threshold as needed
+    
+    if (window.scrollY > this.window.innerHeight/3) { // Adjust scroll threshold as needed
         header.classList.add("scrolled");
+       
+        
     } else {
         header.classList.remove("scrolled");
     }
@@ -14,16 +19,13 @@ function toggleMenu() {
 
 const title=document.querySelector('.title')
 const tagElement = document.querySelector(".tag");
-
+const nav=document.getElementById("home-nav");
     document.addEventListener('scroll',function()
 {
-
 let value = window.scrollY
-     console.log(value)
-    title.style.marginTop = value + 'px'
-
-  
-    if (window.scrollY > 10) {
+const slow_up=value*0.2;
+    title.style.marginTop = -slow_up + 'px'
+    if (window.scrollY > 0) {
         tagElement.style.display = "none"; // Hide the tag on scroll
     } else {
         tagElement.style.display = "block"; // Show the tag when at the top
